abf23efb2070caf079aa0494b232443b5f60def1
lisns <z.z.iz.0pnup@gmail.com>
2020-03-17 13:52:44 +0900

    メインページの作成と表示設定、構成要素の作成と処理の作成

---
+++ src/components/Main.vue
@@ -0,0 +1,46 @@
+/*
+  refs.
+  乗算
+  https://lab.syncer.jp/Web/JavaScript/Snippet/16/
+  小数桁指定切り上げ
+  https://qiita.com/nagito25/items/0293bc317067d9e6c560
+ */
+<template>
+  <div class="content">
+    height: <input type="text" v-model="height" /> cm
+    weight: <input type="text" v-model="weight" /> kg
+    <p>{{ height }}, {{ weight }}</p>
+    <button @click="calc">BMIを計算！</button>
+    <p>あなたのBMIは {{ bmi }} です。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {
+      height: "",
+      weight: "",
+      bmi: "",
+    }
+  },
+  methods: {
+    calc(event) {
+      if (!this.height) {
+        console.log("Height is empty");
+        return;
+      }
+      if (!this.weight) {
+        console.log("Weight is empty");
+        return;
+      }
+      this.bmi = +this.weight / Math.pow(+this.height / 100, 2);
+      this.bmi = Math.round(this.bmi * 10) / 10;
+    },
+  }
+}
+</script>
+
+<style>
+</style>
